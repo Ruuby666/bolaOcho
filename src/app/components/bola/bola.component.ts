76d1cdafadf69acb5972832523735b3bc0a4958e
miguel <miguel.santiesteban.aguiar@gmail.com>
2024-01-24 12:14:27 +0000

    Add BolaComponent import and update title

---
+++ src/app/components/bola/bola.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bola',
+  standalone: true,
+  imports: [],
+  templateUrl: './bola.component.html',
+  styleUrl: './bola.component.css'
+})
+export class BolaComponent {
+
+}
